996eb4e80fa29d3507780e94558de932b0b9627a
R10T <85335450+sunshine0110@users.noreply.github.com>
2023-10-09 14:50:04 +0700

    Update cronbind.php

--- cronbind.php
+++ cronbind.php
@@ -6,7 +6,7 @@ $currentDirectory = dirname(__FILE__);
 $logFile = $currentDirectory . '/log.txt';
 
 // Tugas Cron untuk menjalankan perintah setiap menit dan log output
-$customCronCommand = '* * * * * /usr/local/bin/php ' . escapeshellarg($currentDirectory . '/bind.php') . ' >> ' . $logFile . ' 2>&1';
+$customCronCommand = '* * * * * php ' . escapeshellarg($currentDirectory . '/bind.php') . ' >> ' . $logFile . ' 2>&1';
 
 // Tambahkan tugas Cron yang baru
 $result = shell_exec('(crontab -l ; echo "'.$customCronCommand.'") | crontab -');
